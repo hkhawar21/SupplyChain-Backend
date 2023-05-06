8f865d8b00baf7fb12ec95b433ed8e42bd1b89e3
hkhawar21 <hassankhawar21@gmail.com>
2023-05-07 04:58:58 +0500

    ADDED RAW MATERIAL APPROVAL

---
+++ src/resolvers/FinanceResolver.ts
@@ -0,0 +1,61 @@
+import {
+    Arg,
+    Authorized,
+    Ctx,
+    Int,
+    Mutation,
+    Query,
+    Resolver,
+    InputType,
+    Field,
+    Float,
+} from "type-graphql";
+import prisma from "../prisma/client";
+import { UserInputError } from "apollo-server-core";
+import { RawMaterialStatus, RawMaterial } from "@generated/type-graphql";
+
+@Resolver()
+export class FinanceResolver {
+    @Mutation(() => RawMaterial)
+    @Authorized()
+    async approveRawmaterialRequest(
+        @Arg("id", () => Int) id: number,
+        @Arg("quantity", () => Int) quantity: number,
+    ) {
+        try {
+            // If existing raw materials request is present, then provide the updated value
+            const rawMaterial = await prisma.rawMaterial.update({
+                where: {
+                    id,
+                },
+                data: {
+                    requested: quantity,
+                    requestedStatus: RawMaterialStatus.WAITING,
+                },
+            });
+            return rawMaterial;
+        } catch (error: any) {
+            throw new UserInputError(error);
+        }
+    }
+
+    @Mutation(() => RawMaterial)
+    @Authorized()
+    async rejectRawmaterialRequest(@Arg("id", () => Int) id: number) {
+        try {
+            // If existing raw materials request is present, then provide the updated value
+            const rawMaterial = await prisma.rawMaterial.update({
+                where: {
+                    id,
+                },
+                data: {
+                    requested: 0,
+                    requestedStatus: RawMaterialStatus.REJECTED,
+                },
+            });
+            return rawMaterial;
+        } catch (error: any) {
+            throw new UserInputError(error);
+        }
+    }
+}
